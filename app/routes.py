dfb1e76debac4cecaddf8fe2eb532cadc29725f7
Laurico Neves <lneves@athenahealth.com>
2020-04-30 23:57:05 -0400

    Adding some style and change the html a bit. Also adding a new way of sending data to the view.

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,38 @@
 from flask import render_template
 from app import app
-from app.models import Elorating 
+from app.models import Elorating
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     eu_rankings = Elorating.query.filter_by(matchtype='sbl_eu_scorelog').order_by(Elorating.rating.desc()).all()
     us_rankings = Elorating.query.filter_by(matchtype='sbl_us_scorelog').order_by(Elorating.rating.desc()).all()
+    countries = [
+        {
+            'header': 'US',
+            'ranking': us_rankings,
+            'days': [
+                'Thursdays 00 GMT (8pm EST)',
+                'Saturdays 21 GMT (5pm EST)',
+            ]
+        },
+        {
+            'header': 'EU',
+            'ranking': eu_rankings,
+            'days': [
+                'Tuesdays 19 GMT (9pm EST)',
+                'Saturdays 15 GMT (5pm EST)',
+            ]
+        }
+    ]
 
-    return render_template('index.html', title = 'Sumobar League', us_rankings = us_rankings, eu_rankings = eu_rankings)
+    return render_template(
+        'index.html',
+        title='Sumobar League',
+        countries=countries
+    )
+
 
 @app.route('/league_info')
 def league_info():
