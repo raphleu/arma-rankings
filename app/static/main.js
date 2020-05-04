6ca5c21b0f86b89958deb8d3e6025fbd52fe248d
Laurico Neves <lneves@athenahealth.com>
2020-05-03 20:19:48 -0400

    Add document read before run JS.

--- app/static/main.js
+++ app/static/main.js
@@ -1,5 +1,8 @@
 
 (function () {
-	var pathname = window.location.pathname;
-	$('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+	$(document).ready(function () {
+		// make the nav active for its current page
+		var pathname = window.location.pathname;
+		$('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+	})
 })();
