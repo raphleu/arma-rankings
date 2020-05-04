83f055a89e8e2ae4806b8f28ac14eda08097511b
Laurico Neves <lneves@athenahealth.com>
2020-05-03 22:06:21 -0400

    Fixed schedule ui, add close navbar button, clean date-card to remove repeated code.

--- app/static/main.js
+++ app/static/main.js
@@ -4,5 +4,10 @@
 		// make the nav active for its current page
 		var pathname = window.location.pathname;
 		$('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+
+		$('#changeToggleNav').click(function() {
+			$('.bar-icon-nav').toggleClass('hidden');
+			$('.close-icon-nav').toggleClass('hidden');
+		});
 	})
 })();
