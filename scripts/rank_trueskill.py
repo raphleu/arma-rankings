51f498d3358819ab1f2aabc6490ae0eec81e7d81
Raphael Leu <raphleu@gmail.com>
2020-09-24 21:28:20 +0200

    cleaned up trueskill script a bit

--- scripts/rank_trueskill.py
+++ scripts/rank_trueskill.py
@@ -23,10 +23,15 @@ Trueskillrating.query.delete()
 MatchScore.query.delete()
 Match.query.delete()
 
-base_rating = 1500
+base_addition = 1500
 multiplier = 23.45
 
-fort_starting_rating = (fort_starting_mu - 3 * fort_sigma) * multiplier + base_rating
+def transform_rating(mu, sigma):
+    return round((mu - 3 * sigma) * multiplier + base_addition, 0)
+
+# using the default mu and sigma here
+starting_rating = transform_rating(25.0, 8.333333333333334)
+fort_starting_rating = transform_rating(fort_starting_mu,fort_sigma)
 
 match_type = ''
 directory_to_scan = '/home/ranking_app/raw_data'
@@ -36,11 +41,10 @@ for filename in listdir(directory_to_scan):
             # do nothing
             print('This is a .txt file. Carrying on.')
             continue
-        elif ('pickup-fortress' in filename):
-            print('Ranking: ' + filename)
-            matches = json.load(f)
-            username_to_rating = {}
-            match_count = 0
+        print('Ranking: ' + filename)
+        matches = json.load(f)
+        username_to_rating = {}
+        if ('pickup-fortress' in filename):
             for match in matches:
                 match_type = match['matchtype']
                 match_date_obj = datetime.datetime.strptime(match['date'], "%Y-%m-%d")
@@ -51,7 +55,6 @@ for filename in listdir(directory_to_scan):
                 )
                 db.session.add(match_data)
                 db.session.flush() # This will give us an ID for the match that has not yet been commited
-                match_count += 1
                 formatted_match = []
                 match_teams = match['teams']
                 sorted_teams = sorted(match_teams, key = lambda x: (match_teams[x]['score']), reverse=True)
@@ -68,7 +71,7 @@ for filename in listdir(directory_to_scan):
                         else:
                             rating = Rating(mu=fort_starting_mu)
                             team_rankings[username] = rating
-                        transformed_rating = round((rating.mu - 3 * rating.sigma) * multiplier + base_rating, 0)
+                        transformed_rating = transform_rating(rating.mu, rating.sigma)
                         match_score = MatchScore(
                             match_id = match_data.id,
                             username = username,
@@ -86,21 +89,14 @@ for filename in listdir(directory_to_scan):
                     for username, rating in team_ratings.items(): 
                         old_rating = fort_starting_rating
                         if (username_to_rating.has_key(username)):
-                            old_rating = username_to_rating[username]['rating'].mu - 3 * username_to_rating[username]['rating'].sigma
-                            old_rating = old_rating * multiplier + base_rating
-                        new_rating = rating.mu - 3 * rating.sigma
-                        new_rating = new_rating * multiplier + base_rating
-                        match_scores[username].exit_rating = round(new_rating, 0)
-                        rating_data = {}
-                        rating_data['latest_delta'] = round(new_rating, 0) - round(old_rating, 0)
+                            old_rating = transform_rating(username_to_rating[username]['rating'].mu, username_to_rating[username]['rating'].sigma)
+                        new_rating = transform_rating(rating.mu, rating.sigma)
+                        match_scores[username].exit_rating = new_rating
+                        rating_data['latest_delta'] = new_rating - old_rating
                         rating_data['latest_delta_date'] = match_date_obj
                         rating_data['rating'] = rating 
                         username_to_rating[username] = rating_data
         else: 
-            print('Ranking: ' + filename)
-            matches = json.load(f)
-            username_to_rating = {}
-            match_count = 0
             for match in matches:
                 if (len(match['match_scores']) > 1):
                     match_type = match['matchtype']
@@ -112,7 +108,6 @@ for filename in listdir(directory_to_scan):
                     )
                     db.session.add(match_data)
                     db.session.flush() # This will give us an ID for the match that has not yet been commited
-                    match_count += 1
                     formatted_match = []
                     place = 1
                     match_scores = {}
@@ -124,7 +119,7 @@ for filename in listdir(directory_to_scan):
                         else:
                             rating = Rating()
                             formatted_match.append({username: rating})
-                        transformed_rating = round((rating.mu - 3 * rating.sigma) * multiplier + base_rating, 0)
+                        transformed_rating = transform_rating(rating.mu, rating.sigma)
                         match_score = MatchScore(
                             match_id = match_data.id,
                             username = username,
@@ -140,41 +135,28 @@ for filename in listdir(directory_to_scan):
                     ratings = env.rate(formatted_match)
                     for rating in ratings:
                         for username, rating in rating.items(): 
-                            old_rating = 1500
+                            old_rating = starting_rating
                             if (username_to_rating.has_key(username)):
-                                old_rating = username_to_rating[username]['rating'].mu - 3 * username_to_rating[username]['rating'].sigma
-                                old_rating = old_rating * multiplier + base_rating
-                            new_rating = rating.mu - 3 * rating.sigma
-                            new_rating = new_rating * multiplier + base_rating
-                            match_scores[username].exit_rating = round(new_rating, 0)
+                                old_rating = transform_rating(username_to_rating[username]['rating'].mu, username_to_rating[username]['rating'].sigma)
+                            new_rating = transform_rating(rating.mu, rating.sigma)
+                            match_scores[username].exit_rating = new_rating
                             rating_data = {}
-                            rating_data['latest_delta'] = round(new_rating, 0) - round(old_rating, 0)
+                            rating_data['latest_delta'] = new_rating - old_rating
                             rating_data['latest_delta_date'] = match_date_obj
                             rating_data['rating'] = rating 
                             username_to_rating[username] = rating_data
-
-# print("Match count: " + str(match_count))
-# 
-# counter = 1
-# for key in sorted(username_to_rating, key = lambda username: username_to_rating[username].mu, reverse = True):
-#     print(str(counter) + ". " + key + ": " + str(username_to_rating[key].mu-(3*username_to_rating[key].sigma)) + ", " + str(username_to_rating[key].mu) + ", " + str(username_to_rating[key].sigma))
-#     counter += 1
-
-
         for key in username_to_rating:
             user = User(username=key)
-            rating = username_to_rating[key]['rating'].mu - 3*username_to_rating[key]['rating'].sigma
-            rating = rating * multiplier + base_rating
+            rating = transform_rating(username_to_rating[key]['rating'].mu, username_to_rating[key]['rating'].sigma)
             trueskillrating = Trueskillrating(
                 username = key,
                 matchtype = match_type,
                 mu = round(username_to_rating[key]['rating'].mu, 2),
                 sigma = round(username_to_rating[key]['rating'].sigma, 2),
-                rating = round(rating, 0),
+                rating = rating,
                 latest_delta = username_to_rating[key]['latest_delta'],
                 latest_delta_date = username_to_rating[key]['latest_delta_date']
             )
             db.session.add(user)
             db.session.add(trueskillrating)
-
 db.session.commit()
