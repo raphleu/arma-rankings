665ee777f06324e6940eada8d891ccc6f4c991bf
Raphael Leu <raphleu@gmail.com>
2020-12-30 15:21:28 +0100

    added endpoints for matchmaking and checking if a player is in the system. also did some refactoring

--- app/routes.py
+++ app/routes.py
@@ -1,105 +1,16 @@
-from app import app, db
+from app import app, db, service
+from app.app_config import schedule_for_us, schedule_for_eu, match_types, match_subtype_to_type
 from app.models import Elorating, Trueskillrating, Match, MatchScore
 from datetime import date, datetime, timedelta
 from flask import render_template, request, url_for
 from google.cloud import secretmanager
+import json
 import os
+import random
 from sqlalchemy import func
 from sqlalchemy.dialects.mssql import INTEGER
 from sqlalchemy.orm import joinedload
 from sqlalchemy.sql.functions import coalesce
-
-
-schedule_for_us = [
-    {
-        'day': 'Thursdays',
-        'utc_time': '00:00'
-    },
-    {
-        'day': 'Saturdays',
-        'utc_time': '21:00'
-    },
-]
-
-schedule_for_eu = [
-    {
-        'day': 'Tuesdays',
-        'utc_time': '19:00'
-    },
-    {
-        'day': 'Saturdays',
-        'utc_time': '19:00'
-    },
-]
-
-match_types = {
-    'leagues': {
-        'sbl-s2': {
-            'header': 'SBL',
-            'title': 'Sumo Bar League',
-            'match_subtype_id': 'sbl-s2',
-            'description': 'Public, ranked sumobar matches, hosted on US and EU servers! Open to anyone, see <a href="/league-info?match_subtype_id=sbl-s2">League Info</a> for how to join. Can you make it to the top?',
-            'banner_image': 'titan_banner2.png',
-            'text_image': 'sbls2text.png'
-        }
-    },
-    'pickup': {
-        'pickup-fortress2': {
-            'header': 'Fort',
-            'title': 'Fortress pickup season 2',
-            'match_subtype_id': 'pickup-fortress2',
-            'description': 'Pickup fortress! Competitive 6v6 gameplay. Sign up on discord in the #pickup channel!',
-            'banner_image': 'fortbannerbg.png',
-            'text_image': 'fortpickuptext.png',
-            'about': 'The ratings here are calculated using an algorithm called Trueskill, invented by microsoft for multiplayer games. Trueskill has many factors that go into it and can be tuned. For example, Trueskill takes into account the strength of your opposing team, so two players with the same number of wins and losses can have different ratings (a loss to a high rated team means less of a hit to your rating than one to a weaker team). Individual score does not matter, purely winning or losing and who you are against. More info can be found <a href="https://trueskill.org/">here</a>. I have tried tuning parameters to work best for this gametype, but if you have suggestions for how they can be improved, please let me (raph) know. <b>Play in 20 or more matches to show up in the rankings.'
-        },
-        'pickup-tst1': {
-            'header': 'TST',
-            'title': 'TST pickup',
-            'match_subtype_id': 'pickup-tst1',
-            'description': 'Pickup TST! Competitive 2v2v2v2 sumo gameplay. Sign up on discord in the #pickup channel!',
-            'banner_image': 'titan_banner3.png',
-            'text_image': 'tstpickuptext.png',
-            'about': 'The ratings here are calculated using an algorithm called Trueskill, invented by microsoft for multiplayer games. Trueskill has many factors that go into it and can be tuned. For example, Trueskill takes into account the strength of your opposing team, so two players with the same number of wins and losses can have different ratings (a loss to a high rated team means less of a hit to your rating than one to a weaker team). Individual score does not matter, purely winning or losing and who you are against. More info can be found <a href="https://trueskill.org/">here</a>. I have tried tuning parameters to work best for this gametype, but if you have suggestions for how they can be improved, please let me (raph) know. <b>Play in 20 or more matches to show up in the rankings.'
-        }
-    },
-    'archive': {
-        'pickup-fortress1': {
-            'header': 'Fort',
-            'title': 'Fortress pickup season 1',
-            'match_subtype_id': 'pickup-fortress1',
-            'description': 'Pickup fortress! Competitive 6v6 gameplay. Sign up on discord in the #pickup channel!',
-            'banner_image': 'fort_bg2.png',
-            'text_image': 'fortpickuptext.png',
-            'about': 'The ratings here are calculated using an algorithm called Trueskill, invented by microsoft for multiplayer games. Trueskill has many factors that go into it and can be tuned. For example, Trueskill takes into account the strength of your opposing team, so two players with the same number of wins and losses can have different ratings (a loss to a high rated team means less of a hit to your rating than one to a weaker team). Individual score does not matter, purely winning or losing and who you are against. More info can be found <a href="https://trueskill.org/">here</a>. I have tried tuning parameters to work best for this gametype, but if you have suggestions for how they can be improved, please let me (raph) know. <b>Play in 20 or more matches to show up in the rankings.'
-        },
-        'sbl-us': {
-            'header': 'US',
-            'title': 'Sumo Bar League US',
-            'match_subtype_id': 'sbl-us',
-            'description': 'Public, ranked sumobar matches, hosted on US servers! Open to anyone, see <a href="/league-info?match_subtype_id=sbl-us">League Info</a> for how to join. Can you make it to the top?',
-            'banner_image': 'titan_banner3.png',
-            'text_image': 'sumobarusatext.png'
-        },
-        'sbl-eu': {
-            'header': 'EU',
-            'title': 'Sumo Bar League EU',
-            'match_subtype_id': 'sbl-eu',
-            'description': 'Public, ranked sumobar matches, hosted on EU servers! Open to anyone, see <a href="/league-info?match_subtype_id=sbl-eu">League Info</a> for how to join. Can you make it to the top?',
-            'banner_image': 'titan_banner1.png', 
-            'text_image': 'sumobartexteu.png'
-        }
-    }
-}
-
-match_subtype_to_type = {
-    'pickup-fortress1': 'archive',
-    'sbl-s2': 'leagues',
-    'sbl-eu': 'archive',
-    'sbl-us': 'archive',
-    'pickup-tst1': 'pickup',
-    'pickup-fortress2': 'pickup'
-}
 
 @app.route('/')
 @app.route('/index')
@@ -197,3 +108,18 @@ def updateMatches():
         return "stuff"
     else:
         return "not stuff"
+
+@app.route('/api/generate-teams', methods=['POST'])
+def generateTeams():
+    data = request.get_json() or {}
+    players = data['players']
+    response = service.generateTeamsService(players)
+    return json.dumps(response)
+
+@app.route('/api/players/exists', methods=['GET'])
+def playerExists():
+    username = request.args.get('username', '').lower()
+    match_subtype_id = request.args.get('match_subtype_id', '')
+    player_query = Trueskillrating.query.filter(func.lower(Trueskillrating.username) == username).filter(Trueskillrating.matchtype == match_subtype_id)
+    player_count = player_query.count()
+    return str(player_count)
---
+++ app/service.py
@@ -0,0 +1,102 @@
+from app import app, db
+from app.models import Elorating, Trueskillrating, Match, MatchScore
+import random
+from sqlalchemy import func
+
+# This should be in a service layer somewhere instead of here. 
+#
+# Here are some play lists I used for test cases:
+# players = ["raph123@forums", "Ampz@forums", "vov@forums", "misterplayer@forums", "Desolate@forums", "veinxat@forums", "garlic@forums", "Soul@forums", "Fini@forums", "Moonlight@forums"]
+# all c tier
+# players = ["Player51@forums", "eezeez@forums", "beitzer@lt", "Soul@forums", "NoahFence@forums", "TattooOG@forums", "garlic@forums", "naiss@forums", "ClundXIII@forums", "Smurf@forums"]
+# with 4 non c tier (should always choose non c tier captains)
+# players = ["raph123@forums", "Ampz@forums", "vov@forums", "misterplayer@forums", "NoahFence@forums", "TattooOG@forums", "garlic@forums", "naiss@forums", "ClundXIII@forums", "Smurf@forums"]
+# with weird casing
+# players = ["Raph123@forums", "ampz@forums", "vov@forums", "misterplayer@forums", "NoahFence@forums", "TattooOG@forums", "garlic@forums", "naiss@forums", "ClundXIII@forums", "Smurf@forums"]
+def generateTeamsService(players):
+    if (len(players) < 2): 
+        return { 
+            'error': 'Too few players signed up.'
+        }
+
+    # Get the "percent rank" for each player passed in
+    subquery = db.session.query(
+        Trueskillrating.username,
+        func.percent_rank().over(
+            order_by=Trueskillrating.rating.desc()
+        ).label('pct-rnk'),
+    ).filter_by(matchtype='pickup-fortress1').subquery()
+    query = db.session.query(subquery).filter(
+        func.lower(subquery.c.username).in_([p.lower() for p in players])
+    )
+    percent_ranked_ratings = query.all()
+
+    s_tiers, a_tiers, b_tiers, c_tiers = ([] for i in range(4))
+
+    s_tier = .1 # top 10% of rankings
+    a_tier = .25 # 10-25%
+    b_tier = .6 # 25-60%
+    c_tier = 1 # 60-100
+
+    # Shuffle the players so that players within a tier are in a random order. We will sort by tier next.
+    random.shuffle(percent_ranked_ratings)
+
+    for rating in percent_ranked_ratings:
+        percent_rank = rating[1]
+        if (percent_rank <= s_tier):
+            s_tiers.append(rating)
+        elif (percent_rank <= a_tier):
+            a_tiers.append(rating)
+        elif (percent_rank <= b_tier):
+            b_tiers.append(rating)
+        elif (percent_rank <= c_tier):
+            c_tiers.append(rating)
+    
+    # Separate s, a, and b tier players for selecting captain, if we have enough from those tiers. 
+    semi_sorted_ratings_sab = s_tiers + a_tiers + b_tiers
+    semi_sorted_ratings_all = s_tiers + a_tiers + b_tiers + c_tiers
+
+    # If we have enough players in s, a and b tiers, randomly choose captains from there
+    if (len(semi_sorted_ratings_sab) > 3):
+        captain_1_index = random.randrange(0, len(semi_sorted_ratings_sab), 2)
+        captain_2_index = captain_1_index + 1
+        captain_2 = semi_sorted_ratings_sab[captain_2_index].username
+        captain_1 = semi_sorted_ratings_sab[captain_1_index].username
+    else: 
+        captain_1_index = random.randrange(0, len(semi_sorted_ratings_all), 2)
+        captain_2_index = captain_1_index + 1
+        captain_2 = semi_sorted_ratings_all[captain_2_index].username
+        captain_1 = semi_sorted_ratings_all[captain_1_index].username
+
+    team_2 = []
+    team_1 = []
+    counter = 2
+
+    # Assign players to teams using abbaabb... approach.
+    while (len(semi_sorted_ratings_all) > 0):
+        if (counter <= 2):
+            team_2.append(semi_sorted_ratings_all.pop(0).username)
+        elif (counter <= 4):
+            team_1.append(semi_sorted_ratings_all.pop(0).username)
+        elif (counter == 5):
+            counter = 0
+        counter += 1
+
+    # Figure out which team our captians are in since we assigned teams after choosing captains.
+    if captain_1 in team_1:
+        team_1_captain = captain_1
+        team_2_captain = captain_2
+    else:
+        team_1_captain = captain_2
+        team_2_captain = captain_1
+
+    return {
+        'team_1': {
+            'captain': team_1_captain,
+            'players': team_1
+        },
+        'team_2': {
+            'captain': team_2_captain,
+            'players': team_2
+        }
+    }
