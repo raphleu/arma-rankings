c87e0af0691e04a8f68ad585a6617789437a659a
Raphael Leu <raphleu@gmail.com>
2020-12-31 15:02:57 +0100

    new pickup fortress all rankings

--- app/app_config.py
+++ app/app_config.py
@@ -36,7 +36,7 @@ match_types = {
             'header': 'Fort',
             'title': 'Fortress pickup season 2',
             'match_subtype_id': 'pickup-fortress2',
-            'description': 'Pickup fortress! Competitive 6v6 gameplay. Sign up on discord in the #pickup channel!',
+            'description': 'Season 2 pickup fortress! Competitive 6v6 gameplay. Sign up on discord in the #pickup channel!',
             'banner_image': 'fortbannerbg.png',
             'text_image': 'fortpickuptext.png',
             'about': 'The ratings here are calculated using an algorithm called Trueskill, invented by microsoft for multiplayer games. Trueskill has many factors that go into it and can be tuned. For example, Trueskill takes into account the strength of your opposing team, so two players with the same number of wins and losses can have different ratings (a loss to a high rated team means less of a hit to your rating than one to a weaker team). Individual score does not matter, purely winning or losing and who you are against. More info can be found <a href="https://trueskill.org/">here</a>. I have tried tuning parameters to work best for this gametype, but if you have suggestions for how they can be improved, please let me (raph) know. <b>Play in 20 or more matches to show up in the rankings.'
@@ -49,7 +49,16 @@ match_types = {
             'banner_image': 'titan_banner3.png',
             'text_image': 'tstpickuptext.png',
             'about': 'The ratings here are calculated using an algorithm called Trueskill, invented by microsoft for multiplayer games. Trueskill has many factors that go into it and can be tuned. For example, Trueskill takes into account the strength of your opposing team, so two players with the same number of wins and losses can have different ratings (a loss to a high rated team means less of a hit to your rating than one to a weaker team). Individual score does not matter, purely winning or losing and who you are against. More info can be found <a href="https://trueskill.org/">here</a>. I have tried tuning parameters to work best for this gametype, but if you have suggestions for how they can be improved, please let me (raph) know. <b>Play in 20 or more matches to show up in the rankings.'
-        }
+        },
+        'pickup-fortress-all': {
+            'header': 'Fort',
+            'title': 'Fortress pickup all time',
+            'match_subtype_id': 'pickup-fortress-all',
+            'description': 'All time pickup fortress rankings! Competitive 6v6 gameplay. Sign up on discord in the #pickup channel!',
+            'banner_image': 'fortbanner3.jpg',
+            'text_image': 'fortpickuptext.png',
+            'about': 'The ratings here are calculated using an algorithm called Trueskill, invented by microsoft for multiplayer games. Trueskill has many factors that go into it and can be tuned. For example, Trueskill takes into account the strength of your opposing team, so two players with the same number of wins and losses can have different ratings (a loss to a high rated team means less of a hit to your rating than one to a weaker team). Individual score does not matter, purely winning or losing and who you are against. More info can be found <a href="https://trueskill.org/">here</a>. I have tried tuning parameters to work best for this gametype, but if you have suggestions for how they can be improved, please let me (raph) know. <b>Play in 20 or more matches to show up in the rankings.'
+        },
     },
     'archive': {
         'pickup-fortress1': {
@@ -59,7 +68,7 @@ match_types = {
             'description': 'Pickup fortress! Competitive 6v6 gameplay. Sign up on discord in the #pickup channel!',
             'banner_image': 'fort_bg2.png',
             'text_image': 'fortpickuptext.png',
-            'about': 'The ratings here are calculated using an algorithm called Trueskill, invented by microsoft for multiplayer games. Trueskill has many factors that go into it and can be tuned. For example, Trueskill takes into account the strength of your opposing team, so two players with the same number of wins and losses can have different ratings (a loss to a high rated team means less of a hit to your rating than one to a weaker team). Individual score does not matter, purely winning or losing and who you are against. More info can be found <a href="https://trueskill.org/">here</a>. I have tried tuning parameters to work best for this gametype, but if you have suggestions for how they can be improved, please let me (raph) know. <b>Play in 20 or more matches to show up in the rankings.'
+            'about': 'The ratings here are calculated using an algorithm called Trueskill, invented by microsoft for multiplayer games. Trueskill has many factors that go into it and can be tuned. For example, Trueskill takes into account the strength of your opposing team, so two players with the same number of wins and losses can have different ratings (a loss to a high rated team means less of a hit to your rating than one to a weaker team). Individual score does not matter, purely winning or losing and who you are against. More info can be found <a href="https://trueskill.org/">here</a>. I have tried tuning parameters to work best for this gametype, but if you have suggestions for how they can be improved, please let me (raph) know.'
         },
         'sbl-us': {
             'header': 'US',
@@ -86,5 +95,6 @@ match_subtype_to_type = {
     'sbl-eu': 'archive',
     'sbl-us': 'archive',
     'pickup-tst1': 'pickup',
-    'pickup-fortress2': 'pickup'
+    'pickup-fortress2': 'pickup',
+    'pickup-fortress-all': 'pickup'
 }
